d0aafa725de0a9da73b8e0e4612d61a054a44061
mikefab <mikefabrikant@gmail.com>
2016-08-21 21:49:39 -0400

    Make sure that json returned from unicefstories is valid. Need to store back up data set for when unicefstories is crashed.

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 var express      = require('express');
 var app          = express();
 
-var apicache     = require('apicache').options({ debug: true }).middleware;
+var apicache  = require('apicache');
+var cache     = apicache.middleware;
 var request_json = require('request-json');
 var client       = request_json.createClient('http://www.unicefstories.org/');
 
@@ -9,12 +10,20 @@ app.use(express.static(__dirname + '/app'));                 // set the static f
 app.use('/bower_components',  express.static(__dirname + '/bower_components'));
 
 // app.get('/stories', function(req, res){
-app.get("/stories", apicache('5 days'), function(req, res){
+app.get('/stories', cache('1 hour'), function(req, res, next) {
   client.get('category/youth-engagement/?json=1', function(err, xxx, body) {
-    console.log(body.posts);
-    res.json(body.posts.slice(0,6));
-  })
-})
+    if (err || !body) {
+      apicache.clear();
+      return res.json([]);
+    }
+    if (Array.isArray(body.posts)) {
+      res.json(body.posts.slice(0,6));
+    } else{
+      apicache.clear();
+      return res.json([]);
+    }
+  });
+});
 
 
 app.listen(process.env.PORT || 3002);
